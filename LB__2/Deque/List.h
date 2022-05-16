1d40115cf8ebcd8495da6fad533bd0828d655839
JekaKuldoshin <JekaKuldoshin@users.noreply.github.com>
2022-05-16 16:03:41 +0300

    +++

--- LB__2/Deque/List.h
+++ LB__2/Deque/List.h
@@ -9,32 +9,31 @@ using namespace std;
 
 struct obj {
 	char data;               //Символьная переменная
-	obj* prev;
-	obj* next;              //Указатель на переход
+	obj* prev;              //Указатель на прошлый элемент
+	obj* next;             //Указатель на следующий элемент
 };
 
 class deque {
 private:
-	obj* left, * right;            //Указатель на верх
+	obj* left, * right;            //Указатель на начало (Лево) и конец (право)
 
 public:
 
 	deque() : left(NULL), right(NULL) {};           //КсП
 
-	void create_front(int num);          //Функ-я для создания нескольких элементов
-	void create_back(int num);
-	void push_front(char a);
-	void push_back(char a);             //Ввод
-	void show();                 //Вывод
+	void create_back(int num);                    //Функ-я для создания нескольких элементов очереди (делает переход на функ-ю push_back)
+	void push_front(char a);                     //Функ-я внесения данных в очередь с начало
+	void push_back(char a);                     //С
+	void show();                               //Вывод
 
-	int count();               //Функ-я для подсчета кол-ва элементов в очереди
-	double average();         //Функ-я для нахождения среднего арифметического
-	char Max();              //Функ-я для нахождения Макс.
-	char Min();             //Функ-я для нахождения Мин.
-	char prevMin();        //Функ-я для нахождения элемента перел минимальным значением
-	void saveInFile();       //Функ-я записи в файл
-	void form_the_file();   //Функ-я чтения с файла
+	int count();                             //Функ-я для подсчета кол-ва элементов в очереди
+	void check_pull_6_elem();               //Функ-я для проверки правильности вывода 6-го элемента очереди
+	char pull_6_elem();                    //Функ-я вывода 6-го элемента очереди
+	char pull_last_elem();                //Функ-я вывода последнего элемента очереди
 
-	bool check();           //Проверка на пустоту
+	void saveInFile();                  //Функ-я записи в файл
+	void form_the_file();              //Функ-я чтения с файла
+
+	bool check();                    //Проверка на пустоту
 
 };
